769d428556687fccc2cfaaae9bd99d30a015a11a
Tanveer Abbas <45366039+qaimabbas@users.noreply.github.com>
2021-02-04 08:21:20 +0100

    Create class.js

---
+++ Components/statefullcomponent/class.js
@@ -0,0 +1,10 @@
+import react, {Component} from 'react';
+
+class Name extends Component{
+     
+    render(){
+      return(<h1>This is from class and stateful component</h1>)
+    }
+  }
+
+  export default Name;
