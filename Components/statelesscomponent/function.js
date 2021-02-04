3ac6736741fe3a6e525e16f08d32f3da463f7644
Tanveer Abbas <45366039+qaimabbas@users.noreply.github.com>
2021-02-04 08:19:56 +0100

    Create function.js

---
+++ Components/statelesscomponent/function.js
@@ -0,0 +1,24 @@
+import react from 'react';
+
+function Message(props){
+    return(
+
+
+          <div >
+              <h1>Tanveer Abbas first Page in React</h1>
+              <h1>stateless component</h1>
+              
+               
+
+          </div>
+        
+        
+        
+        
+        
+        
+        
+        );
+  };
+
+   export default Message;
